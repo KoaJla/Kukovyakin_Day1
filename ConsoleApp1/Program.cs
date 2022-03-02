3576cf7eb42ded98a4f863a349fef1ac42e9f4f0
Оленька <@DESKTOP-NM5HLCN>
2022-03-03 00:35:26 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            if (n % 2 != 0)
+            {
+                string str = new String('*', n);
+                string str_1 = "";
+                Console.WriteLine(str);
+                int i = 0;
+                int midle = Convert.ToInt32(Math.Round(Convert.ToDouble(n / 2)));
+                while (i != n - 2)
+                {
+                    if (i == midle - 1)
+                    {
+                        for (int j = 0; j < n; j++)
+                        {
+                            if (j == midle)
+                            {
+                                str_1 += " ";
+                            }
+                            else
+                            {
+                                str_1 += "*";
+                            }
+                        }
+                        Console.WriteLine(str_1);
+                        i++;
+                    }
+                    else
+                    {
+                        Console.WriteLine(str);
+                        i++;
+                    }
+                }
+                Console.WriteLine(str);
+            }
+            else
+            {
+                Console.WriteLine("Введите нечетное число");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static string getSequence(int number)
+        {
+            string result = "";
+
+            for (int i = 0; i <= number; i++)
+            {
+                result += i + " ";
+            }
+            return result;
+        }
+
+        static void getResult(string result)
+        {
+            Console.WriteLine(result);
+        }
+
+        static void Main(string[] args)
+        {
+            string result;
+            Console.WriteLine("Введите число");
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+                result = getSequence(number);
+                getResult(result);
+            }
+            catch
+            {
+                Console.WriteLine("Вы ввели неверное значение");
+            }
+            Console.ReadLine();
+        }
+    }
+}
